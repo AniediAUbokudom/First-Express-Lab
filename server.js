1498a50ddc766a0c90f896a9940428473665ac67
Aniedi Ubokudom <aniedi.anthonyu@yahoo.com>
2023-09-16 23:45:36 -0400

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/greeting', (req, res) => {
     res.send('Hello, stranger')
   });
 
+  //greeting route with param
   app.get('/greeting/:name', (req, res) => {
     let expression1= "How are you, "
     let expression2= "?"
     res.send(expression1 + req.params.name + expression2);
 })
+
+//tip calculator router
+app.get('/tip/:total/:percentage', (req, res) => {
+	res.send(
+		`${calculateTip(req.params.total, req.params.percentage)}`
+	);
+});
+function calculateTip(total, percentage) {
+	return total * (percentage / 100);
+};
+
+
 
 
 
