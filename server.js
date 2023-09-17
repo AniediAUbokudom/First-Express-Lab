1e4cd0069a3453bbac2d165f10e37fc7c943bc7a
Aniedi Ubokudom <aniedi.anthonyu@yahoo.com>
2023-09-17 19:02:14 -0400

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -2,6 +2,31 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+//magic 8 ball responses
+const ballResponses= [
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'Yes definitely',
+    'You may rely on it',
+    'As I see it yes',
+    'Most likely',
+    'Outlook good',
+    'Yes',
+    'Signs point to yes',
+    'Reply hazy try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    "Don't count on it",
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful'
+  ];
+  
+
 app.get('/greeting', (req, res) => {
     res.send('Hello, stranger')
   });
@@ -22,6 +47,27 @@ app.get('/tip/:total/:percentage', (req, res) => {
 function calculateTip(total, percentage) {
 	return total * (percentage / 100);
 };
+
+//Magic ball
+app.get('/magic/:question', (req, res) => {
+	res.send(
+		`<p>${req.params.question}</p> <p>${
+			ballResponses[randomNumber(ballResponses.length)]
+		}</p>`
+	);
+});
+function randomNumber(max) {
+	return Math.floor(Math.random() * max);
+}
+
+
+
+
+
+
+
+
+
 
 
 
